005eff156d31d0808a3d7ebf1b2d6d5736c87306
Cesar Melo <csrmds@gmail.com>
2026-05-14 12:36:37 -0300

    correção erros build

---
+++ src/app/(app)/admin/workflow/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import * as React from "react";
+// import { useRouter } from "next/navigation";
+// import axios from "axios";
+import { PageHeader } from "@/components/shared/page-header";
+
+export default function WorkflowEdit() {
+	return (
+		<div className="max-w-4xl space-y-6">
+			<PageHeader title="Editar Workflow" />
+		</div>
+	)
+}
